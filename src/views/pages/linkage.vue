ccee40533e53045f08b165e8c2eac26ea7f45c22
lzg <michael_lizg@163.com>
2018-10-18 20:48:45 +0800

    增加中国省市数据

--- src/views/pages/linkage.vue
+++ src/views/pages/linkage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-linkage
+    <div class="tips">element-china-area-data：Element UI Cascader级联选择器 中国省市区三级、二级联动option数据 。 访问地址：<a href="https://github.com/Plortinus/element-china-area-data" target="_blank" class="c1">element-china-area-data</a>
+    </div>
   </div>
 </template>
 
@@ -11,5 +12,7 @@ export default {
 </script>
 
 <style scoped>
-
+.tips{
+  padding: 15px 0;
+}
 </style>
