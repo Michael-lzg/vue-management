db9e904a44b21856cd00f68402028fdc9c074cb2
lzg <michael_lizg@163.com>
2019-02-24 23:19:03 +0800

    配置scss

--- src/views/pages/adminIndex.vue
+++ src/views/pages/adminIndex.vue
@@ -1,16 +1,40 @@
 <template>
-  <div>
-    系统首页
+  <div class="mainpage clearfix">
+    <div class="left-block fl">
+      <div class="left-top-block"></div>
+      <div class="left-bot-block"></div>
+    </div>
+    <div class="right-block fr">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'adminIndex'
-
 }
 </script>
 
 <style scoped>
-
+.mainpage {
+  height: calc(100% - 120px);
+}
+.left-block {
+  width: 30%;
+  height: 100%;
+}
+.left-top-block {
+  height: 40%;
+  background-color: white;
+}
+.left-bot-block {
+  margin-top: 15px;
+  height: 58%;
+  background-color: white;
+}
+.right-block {
+  width: 68%;
+  height: 100%;
+  background-color: white;
+}
 </style>
